56b284a4d67d79bbfa6c42c730fbf1664f8c8a62
0xjayb <jasonboulos0@gmail.com>
2024-10-30 20:15:05 +0200

    first time submitting

---
+++ pyramid.py
@@ -0,0 +1,9 @@
+selected_number = int(input('Please select the number of rows (n):'))
+
+for x in range(1,selected_number + 1):
+    for y in range(1,x + 1):
+        print(y,end ='')
+    print()
+   
+    
+   
